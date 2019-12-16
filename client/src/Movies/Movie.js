ab48dde64eb5d21950ebb8dc2f53d941907250ba
mpaolodr <mpaolodr_1504@yahoo.com>
2019-12-16 17:36:11 -0500

    added MovieCard component and replaced code on Movie.js and MovieList.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import MovieCard from "./MovieCard";
 
 const Movie = props => {
   const [movie, setMovie] = useState();
@@ -32,25 +33,9 @@ const Movie = props => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie} />
       <div className="save-button">Save</div>
     </div>
   );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+import MovieCard from "./MovieCard.js";
+
 const MovieList = props => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
@@ -31,25 +33,8 @@ const MovieList = props => {
 };
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+  // const { title, director, metascore, stars } = movie;
+  return <MovieCard movie={movie} />;
 }
 
 export default MovieList;
